cecf6b6deeb4efaa2bb3a305d280dd97bf3aec58
Alexis <alexis.juarez@msw.com.mx>
2015-12-04 08:27:32 -0600

    First push

---
+++ oWinLogginMethods/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="oWinLogginMethods.MvcApplication" Language="C#" %>
---
+++ oWinLogginMethods/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oWinLogginMethods.Startup))]
+namespace oWinLogginMethods
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
